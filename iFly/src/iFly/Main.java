ec701ba126bda3aae9f4eda8bd308a50d73e269f
uzi <uzi0078@gmail.com>
2020-06-01 18:29:12 +0300

    com

--- iFly/src/iFly/Main.java
+++ iFly/src/iFly/Main.java
@@ -6,13 +6,10 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-
-
 		for(int j =0 ; j<10 ; j++)
-			System.out.println("*");
-
+			System.out.println("**");
 		
-
+		
 	}
 
 }
