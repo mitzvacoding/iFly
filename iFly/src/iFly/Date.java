59b19dfcb34dde435cb6eb646f4954345ac1b0a6
Meir Ariel <meirarielb@gmail.com>
2020-06-09 17:53:50 +0300

    UI-search page- sending data

--- iFly/src/iFly/Date.java
+++ iFly/src/iFly/Date.java
@@ -1,6 +1,7 @@
 package iFly;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class Date implements Serializable {
 	
@@ -17,6 +18,13 @@ public class Date implements Serializable {
 		this.day = day;
 		this.month = month;
 		this.year = year;
+	}
+	
+	public Date(String str)
+	{
+		this.day = (str.charAt(0)-'0')*10 + (str.charAt(1)-'0');
+		this.month = (str.charAt(3)-'0')*10 +str.charAt(4)-'0';
+		this.year = (str.charAt(6)-'0')*1000 + (str.charAt(7)-'0')*100 + (str.charAt(8)-'0')*10 + str.charAt(9)-'0';
 	}
 	
 	public String print()
--- iFly/src/iFly/Main.java
+++ iFly/src/iFly/Main.java
@@ -20,24 +20,28 @@ import UI.RegistarPage;
 import UI.SearchPage;
 
 public class Main {
-	
+	public static int convertString(String str)
+	{
+		int size= str.length();
+		int num=0;
+		for(int i= 0; i<size ; i++)
+		{
+			num += (str.charAt(i)-'0') * Math.pow(10, size - i-1);
+		}
+		return num;
+	}
 	public static void main(String[] args) throws IOException
 
 	{
 		//MainPage.MpFun();
-		SearchPage.SpFun();
+		//SearchPage.SpFun();
 		String fileName = "try1.txt";
 		ObjectOutputStream oos = null;
-		try 
-		{
-			oos = new ObjectOutputStream(new FileOutputStream(fileName));
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}		
-		enter2File();
-		readFromFile();
+		int x = convertString("45123");
+
+		System.out.println(x);
+		//enter2File();
+		//readFromFile();
 	}
 	
 	public static void enter2File()
